978fa6a9951495d7429e27d4afa80c08710c49c3
MananPatel-2708 <133824233+MananPatel-2708@users.noreply.github.com>
2023-05-24 17:26:28 -0400

    Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -13,6 +13,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author dancye
+ * modified by Manan Patel  Student ID-991698490
  */
 public class Card {
 
