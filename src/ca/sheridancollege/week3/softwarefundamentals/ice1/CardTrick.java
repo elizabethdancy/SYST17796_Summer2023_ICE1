96d6f1eec554195e336e2d0a5c6abf48a2b64d12
silwala <silwala@sheridancollege.ca>
2023-05-24 13:49:35 -0400

    Code complete

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * Modified by Aditya Kumar Silwal - 991703219
  */
 public class CardTrick {
     
