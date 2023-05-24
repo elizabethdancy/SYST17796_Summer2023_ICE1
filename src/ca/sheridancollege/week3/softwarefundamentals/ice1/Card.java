26b574ddeacf1d06c0c0a6e70b7718dd364b9785
Vraj-20 <133669562+Vraj-20@users.noreply.github.com>
2023-05-24 13:13:16 -0400

    Update Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -11,7 +11,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
+ * modifier Vraj Contractor , Student ID - 991688064
  * @author dancye
  */
 public class Card {
@@ -19,7 +19,7 @@ public class Card {
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+   public static final String [] SUITS = {"Clubs", "Diamonds", "Spades", "Hearts"};
     /**
      * @return the suit
      */
