ddecbcd23624e5d30e4530c3891e55f68ec99bfb
MananPatel-2708 <133824233+MananPatel-2708@users.noreply.github.com>
2023-05-24 17:58:21 -0400

    CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,23 +10,44 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * modified by Manan Patel  Student ID-991698490
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
+ 
         Card[] magicHand = new Card[7];
-        
+        Random rand = new Random();        
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(rand.nextInt(13)+1);
+            c.setSuit(Card.SUITS[(rand.nextInt(3))+1]);
+            magicHand[i]= c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        
+        Card luckyCard = new Card();
+        luckyCard.setValue(5);
+        luckyCard.setSuit("Hearts");
+        
+        
+        
+        boolean i = false;
+        for (int i=0; i<magicHand.length; i++) {
+            if ( luckyCard.getValue() == magicHand[i].getValue() && luckyCard.getSuit().equals(magicHand[i].getSuit()) ) {
+                    flag=true;
+                    break;
+                    }
+                }
+        
+   
+        
+        if (i==true)
+            System.out.println("Your card is in the Magic Hand: ");
+        if (i== false) 
+            System.out.println("Your card is not in the Magic Hand: ");
     }
     
 }
