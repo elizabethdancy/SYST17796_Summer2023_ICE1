5888f1c2f25de02a05fd42753716adaf67b81f20
silwala <silwala@sheridancollege.ca>
2023-05-24 13:44:25 -0400

    Code complete

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -15,18 +17,67 @@ public class CardTrick {
     
     public static void main(String[] args)
     {
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
-        {
+        for (int i=0; i<magicHand.length; i++){
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(rand.nextInt(13) + 1);
+            c.setSuit(Card.SUITS[rand.nextInt(4)]);
+            magicHand[i] = c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        Card userCard = new Card();       
+        int userValue = 0;
+        int userSuit = 0;
+        
+        boolean repeat = true;
+        while(repeat){
+            try{
+                System.out.print("Enter the value of your card: ");
+                userValue = sc.nextInt();
+                if(userValue <= 0 || userValue > 13){
+                    throw new Exception("Value should be between 1 and 13");
+                }
+                repeat = false;
+            }catch(Exception e){
+                System.out.println(e);
+                sc.nextLine();
+            }
+        }
+        
+        repeat = true;
+        while(repeat){
+            try{
+                System.out.print("Enter your suit(0 = Hearts, 1 = Diamonds, 2 = Spades, 3 = Clubs): ");
+                userSuit = sc.nextInt();
+                if(userSuit < 0 || userSuit > 3 ){
+                    throw new Exception("Not a valid selection");
+                }
+                repeat = false;
+            }catch(Exception e){
+                System.out.println(e);
+                sc.nextLine();
+            }
+        }      
+        userCard.setValue(userValue);
+        userCard.setSuit(Card.SUITS[userSuit]);
+        
+        boolean existsInMagicHand = false;
+        
+        for(Card i: magicHand){
+            if(userCard.getValue() == i.getValue() && userCard.getSuit().equals(i.getSuit())){
+                existsInMagicHand = true;
+                break;
+            }
+        }
+        if(existsInMagicHand){
+            System.out.println("The card exists in magic hand!");
+        }
+        else{
+            System.out.println("The card doesn't exist in magic hand!");
+        }
     }
     
 }
