57065bd51e542a5e14c743315e4f56c13bdc53a3
Vraj-20 <133669562+Vraj-20@users.noreply.github.com>
2023-05-24 12:52:09 -0400

    Updated CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,23 +10,40 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifier Vraj Contractor 
  */
 public class CardTrick {
     
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+    public static void main(String[] args) {
+    Card[] magicHand = new Card[7];
+    Random random = new Random();
+
+    for (int i = 0; i < magicHand.length; i++) {
+        Card c = new Card();
+        c.setValue(random.nextInt(13) + 1);
+        c.setSuit(Card.SUITS[random.nextInt(4)]);
+        magicHand[i] = c;
     }
+
+    // Create your lucky card with chosen number and suit
+    Card luckyCard = new Card();
+    luckyCard.setValue(7);  // Replace 7 with your chosen card number
+    luckyCard.setSuit("Hearts");  // Replace "Hearts" with your chosen card suit
+
+    boolean found = false;
+    for (Card card : magicHand) {
+        if (card.equals(luckyCard)) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
+        System.out.println("Your card is in the magic hand!");
+    } else {
+        System.out.println("Your card is not in the magic hand.");
+    }
+}
+
     
 }
