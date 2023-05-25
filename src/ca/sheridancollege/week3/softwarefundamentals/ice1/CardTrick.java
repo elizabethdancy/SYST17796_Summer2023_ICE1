8cf5d4d1265177f5cfe5f506c27d41fef584a356
Sandesh Gautam <133888896+sandeshgautam@users.noreply.github.com>
2023-05-24 23:25:10 -0400

    Updated CardTrick.java
    did the update

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -24,9 +24,7 @@ public class CardTrick {
         
         for (int i=0; i<magicHand.length; i++)
         {
-            for (int j=0; j<magicHand.length; j++)
-            {
-                Card c = new Card();
+            Card c = new Card();
             c.setValue(randominput.nextInt(13) + 1);
             c.setSuit(Card.SUITS[randominput.nextInt(4)]);
             magicHand[i]=c;
@@ -35,41 +33,11 @@ public class CardTrick {
         
         
         Card luckyCard = new Card();
-        int luckyCardValue = 0;
-        int luckySuit = 0;
+        int luckyCardValue = 7;
+        String luckySuit = "Hearts";
+        luckyCard.setValue(luckyValue);
+        luckyCard.setValue(luckySuit);
          
-        boolean repeat = true;
-        while(repeat){
-            try{
-                System.out.println("Enter the value for card: ");
-                luckyCardValue = newInput.nextInt();
-                if(luckyCardValue <= 0 || luckyCardValue > 13){
-                    throw new Exception("Enter Value between 1 and 13");
-                }
-                repeat = false;
-            }catch(Exception e){
-                System.out.println(e);
-                newInput.nextLine();
-            }
-        }
-        
-        repeat = true;
-        while(repeat){
-            try{
-                System.out.print("Enter the value for suit( 0 = Hearts, 1 = Diamonds, 2 = Spades, 3 = Clubs): ");
-                luckySuit = newInput.nextInt();
-                if(luckySuit < 0 || luckySuit > 3){
-                    throw new Exception("Not a valid selection");
-                }
-                repeat = false;
-            }catch(Exception e){
-                System.out.println(e);
-                newInput.nextLine();
-            }
-        }
-        
-        luckyCard.setValue(luckyCardValue);
-        luckyCard.setSuit(Card.SUITS[luckySuit]);
         
         boolean inMagicHand = false;
         
