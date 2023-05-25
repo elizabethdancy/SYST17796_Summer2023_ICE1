9589302995a3a5a6d052978521965e6f858ea3b0
princekumar-rajput <rajpprin@sheridanc.on.ca>
2023-05-24 21:44:04 -0400

    #Week3Activity

---
+++ SYST17796_Summer2023_ICE1-master/src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+/**
+ * A class that models playing card Objects. Cards have 
+ * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers.
+ * This code is to be used in ICE1. When you create your own branch,
+ * Student Name: Princekumar Rajput
+ * Student Number: 991698363
+ * Date Modified: 24th May, 2023
+ * @author dancye
+ */
+public class Card {
+
+    private String suit; // clubs, spades, diamonds, hearts
+    private int value; // 1-13
+
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Generates a random card value.
+     *
+     * @return a random card value between 1 and 13 (inclusive)
+     */
+    public static int generateRandomValue() {
+        // Use the Math.random() method to generate a random number between 1 and 13 (inclusive)
+        return (int) (Math.random() * 13) + 1;
+    }
+
+    /**
+     * Generates a random card suit.
+     *
+     * @return a random card suit from the SUITS array
+     */
+    public static String generateRandomSuit() {
+        // Use the Math.random() method to generate a random number between 0 and 3 (inclusive)
+        int index = (int) (Math.random() * 4);
+        return SUITS[index];
+    }
+    // Create a hard-coded lucky card
+    public static Card createLuckyCard() {
+        Card luckyCard = new Card();
+        luckyCard.setValue(13);
+        luckyCard.setSuit("diamonds");
+        return luckyCard;
+    }
+}
---
+++ SYST17796_Summer2023_ICE1-master/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+import java.util.Scanner;
+
+/**
+ * A class that fills a magic hand of 7 cards with random Card Objects
+ * and then asks the user to pick a card and searches the array of cards
+ * for the match to the user's card. To be used as starting code in ICE 1
+ * Student Name: Princekumar Rajput
+ * Student Number: 991698363
+ * Date Modified: 24th May, 2023
+ *
+ * Modified by [Your Name]
+ * Student Number: [Your Student Number]
+ */
+public class CardTrick {
+
+    public static void main(String[] args) {
+        Card[] magicHand = new Card[7];
+
+        for (int i = 0; i < magicHand.length; i++) {
+            Card random = new Card();
+            random.setValue(Card.generateRandomValue());
+            random.setSuit(Card.generateRandomSuit());
+            magicHand[i] = random;
+        }
+
+        // Ask the user for Card value and suit
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the value of your card (1-13): ");
+        int valueOfUser = input.nextInt();
+        System.out.print("Enter the suit of your card (Hearts, Diamonds, Spades, Clubs): ");
+        String userSuit = input.next();
+
+        // Create the user's card
+        Card userCard = new Card();
+        userCard.setValue(valueOfUser);
+        userCard.setSuit(userSuit);
+
+        // Search magicHand for the user's card
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == userCard.getValue() && card.getSuit().equalsIgnoreCase(userCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+
+        // Report the result
+        if (found) {
+            System.out.println("Congratulations! Your card is in the magic hand.");
+        } else {
+            System.out.println("Sorry! Your card is not in the magic hand.");
+        }
+
+        // Create the lucky card
+        Card luckyCard = Card.createLuckyCard();
+
+        // Search magicHand for the lucky card
+        found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == luckyCard.getValue() && card.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+
+        // Report the result for the lucky card search
+        if (found) {
+            System.out.println("Congratulations! The lucky card is in the magic hand.");
+        } else {
+            System.out.println("Sorry! The lucky card is not in the magic hand.");
+        }
+    }
+}
